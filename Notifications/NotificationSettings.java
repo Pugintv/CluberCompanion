4f90decd85192cd47d8bd7bf8e74953f01e9fa08
Victor Rosas <Pugintv@users.noreply.github.com>
2018-10-14 22:00:14 -0700

    Update NotificationSettings.java
    Removed Sensitive info for public access

--- Notifications/NotificationSettings.java
+++ Notifications/NotificationSettings.java
@@ -4,7 +4,7 @@ package com.lendasoft.clubercompanion.Notifications;
  * Created by victorrosas on 5/24/16.
  */
 public class NotificationSettings {
-    public static String SenderId = "1030575193492";
-    public static String HubName = "CluberCompanionHub"; //"CluberNotificationHub";
-    public static String HubListenConnectionString = "Endpoint=sb://clubercompanionhub.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=TV6SLD3eqQTs4QJGipKh3nC/fV4NLq8+kMghWLdn/A0=" ;//"Endpoint=sb://cluber.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=3evOdaCCcJQrPSAJ04DUZUnnlmBvETTeBroytv88Ebk=";
+    public static String SenderId = "Senderid";
+    public static String HubName = "HubName";
+    public static String HubListenConnectionString = "ConnectionString";
 }
